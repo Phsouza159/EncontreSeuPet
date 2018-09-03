85b870ceb8e94c2b3d20900063d76ab9b13c443d
Phsouza159 <pauloh159@live.com>
2018-09-03 01:39:29 -0300

    inclusão de arquivos ao projeto - classe de controle e de conexao e suas pastas

---
+++ APP/assets/modal/Modal.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Description of Modal
+ *  classe que ira fazer a conexao com o banco de dados 
+ * @author paulo-pc
+ */
+namespace Conexao
+{
+    
+    class Modal {
+        //put your code here
+    }
+}
